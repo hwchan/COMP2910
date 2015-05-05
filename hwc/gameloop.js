0b60662fb787ea3717a4518240c1eac7d73e2611
limbermuffin <limbermuffin@gmail.com>
2015-05-05 14:51:26 -0700

    indenting fix

--- hwc/gameloop.js
+++ hwc/gameloop.js
@@ -1,36 +1,34 @@
+//starts game loop and repaint function
+init();
+paint();
 
-	//starts game loop and repaint function
-	init();
-    paint();
+function init() {	
+    //updates game logic every 100ms
+    game_loop = setInterval(step, 100);
 
-	function init() {	
-		//updates game logic every 100ms
-		game_loop = setInterval(step, 100);
-        
-	}
-	
-	//update game logic
-	function step() {
-		//updates student positions
-		for(var i = 0; i < students.length; ++i) {
-            stepStudent(i);
-		}
-	}
-    
-    //draws current state
-	function paint() {
-        requestAnimationFrame(paint);
-		//paints map
-		for(var y = 0; y < tiles[0].length; ++y) {
-			for(var x = 0; x < tiles.length; ++x) {
-				drawTile(y,x);
-			}
-		}
-		//paints GUI
-		paintGUI();
-		//paints students
-		for(var i = 0; i < students.length; ++i) {
-			drawStudent(i);
-		}
-	}
+}
 
+//update game logic
+function step() {
+    //updates student positions
+    for(var i = 0; i < students.length; ++i) {
+        stepStudent(i);
+    }
+}
+
+//draws current state
+function paint() {
+    requestAnimationFrame(paint);
+    //paints map
+    for(var y = 0; y < tiles[0].length; ++y) {
+        for(var x = 0; x < tiles.length; ++x) {
+            drawTile(y,x);
+        }
+    }
+    //paints GUI
+    paintGUI();
+    //paints students
+    for(var i = 0; i < students.length; ++i) {
+        drawStudent(i);
+    }
+}
