1cf352b55dc7d93b716336ee6acf63e0eb433003
Howie Chan <hw.chan@hotmail.com>
2015-05-05 15:05:16 -0700

    Refactored sign GUI

--- hwc/GUI.js
+++ hwc/GUI.js
@@ -1,3 +1,16 @@
+	//sets up sign images for GUI
+	var signWidth = 32;
+	var signHeight = 32;
+	
+	var northGUIImg = new Image();
+	northGUIImg.src = "images/up.png";
+	var eastGUIImg = new Image();
+	eastGUIImg.src = "images/right.png";
+	var southGUIImg = new Image();
+	southGUIImg.src = "images/down.png";
+	var westGUIImg = new Image();
+	westGUIImg.src = "images/left.png";
+	
 	//sets cell width
 	var cw = w/(tiles[0].length+2);
 	//sets up GUI vars
@@ -7,7 +20,14 @@
 	pauseBtn.src = "images/pause.png";
 	//x-coordinate of the GUI area
 	var GUIx = cw*tiles[0].length;
-
+	
+	//set buttons
+	var NORTH_BTN = {img:upImg, x:GUIx+15, y:70, width:signWidth, height:signHeight};
+	var EAST_BTN = {img:rightImg, x:GUIx+15, y:120, width:signWidth, height:signHeight};
+	var SOUTH_BTN = {img:downImg, x:GUIx+15, y:170, width:signWidth, height:signHeight};
+	var WEST_BTN = {img:leftImg, x:GUIx+15, y:220, width:signWidth, height:signHeight};
+	var SIGN_BTNS = [NORTH_BTN, EAST_BTN, SOUTH_BTN, WEST_BTN];
+	
 	function paintGUI() {
 		ctx.fillStyle = "dimgray";		
 		ctx.fillRect(GUIx, 0, cw*2, h);
@@ -19,8 +39,8 @@
 		ctx.fillText("Time: " + time, GUIx+5, 45);
 		ctx.fillText("Score: " + score, GUIx+5, 35);
 		//draw signs
-		ctx.drawImage(upImg, GUIx+15, 70);
-		ctx.drawImage(rightImg, GUIx+15, 120);
-		ctx.drawImage(downImg, GUIx+15, 170);
-		ctx.drawImage(leftImg, GUIx+15, 220);
+		ctx.drawImage(NORTH_BTN.img, NORTH_BTN.x, NORTH_BTN.y);
+		ctx.drawImage(EAST_BTN.img, EAST_BTN.x, EAST_BTN.y);
+		ctx.drawImage(SOUTH_BTN.img, SOUTH_BTN.x, SOUTH_BTN.y);
+		ctx.drawImage(WEST_BTN.img, WEST_BTN.x, WEST_BTN.y);
 	}
--- hwc/signs.js
+++ hwc/signs.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
 	//create a sign on click
-	$( "#canvas" ).mousedown(function(e) {
-		tempX = e.pageX;
-		tempY = e.pageY;
-		if(tiles[Math.floor(tempY/(cw+1))][Math.floor(tempX/(cw+1))]!=5){
-			tiles[Math.floor(tempY/(cw+1))][Math.floor(tempX/(cw+1))]=3;
+	var clickX;
+	var clickY;
+	$("#canvas").mousedown(function(e) {
+		clickX=e.pageX
+		clickY=e.pageY
+	}).mouseup(function(e2){
+		var tempTileX = Math.floor(e2.pageX/(cw+1));
+		var tempTileY = Math.floor(e2.pageY/(cw+1));
+		if(tiles[tempTileY][tempTileX]!=5){
+			//check which sign button is pressed
+			for(i=0; i<4; i++){
+				if(clickX>=SIGN_BTNS[i].x && clickX <= SIGN_BTNS[i].x+SIGN_BTNS[i].width 
+				&& clickY>=SIGN_BTNS[i].y && clickY<=SIGN_BTNS[i].y+SIGN_BTNS[i].height){
+					//assign the selected sign to the tile at the cursor
+					tiles[tempTileY][tempTileX]=i+1;
+				}
+			}
 		}
 	})
 })
