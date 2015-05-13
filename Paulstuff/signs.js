a4908487614e3d41e73db562fd15fb19fa14188f
Paul <mccarliep@gmail.com>
2015-05-12 17:46:58 -0700

    Touch implementation finalish
    Touch now works with IE click and touch

--- Paulstuff/signs.js
+++ Paulstuff/signs.js
@@ -29,6 +29,50 @@ function drawHighlight() {
 //checks if mouse is inside any of the four sign buttons when clicked,
 //if yes sets highlight to true and saves which sign was pressed
 var box1 = document.getElementById('canvas')
+if (window.navigator.msPointerEnabled) {
+	alert("yes");
+box1.addEventListener("MSPointerDown", function(e){
+
+    for (var i = 0; i < 4; i++){
+        if((e.pageX >= SIGN_BTNS[i].x && e.pageX <= (SIGN_BTNS[i].x+cw)) && (e.pageY >= SIGN_BTNS[i].y && e.pageY <= (SIGN_BTNS[i].y+cw))) {
+            highlight = true;
+            signPressed = i+1;
+				e.preventDefault();
+        }
+    }
+	overX = Math.floor(e.pageX / cw);
+    overY = Math.floor(e.pageY / cw);
+	//delete sign if clicked
+	if(gameboard[overY][overX].contents == 1 || gameboard[overY][overX].contents == 2 || gameboard[overY][overX].contents == 3 || gameboard[overY][overX].contents == 4){
+		gameboard[overY][overX].contents = 0;
+	}
+},false)
+
+//checks if a tile can be set at the current cursor position, 
+//if it can be placed it places the tile stored
+//
+//it then sets the tile stored to an empty space and removes highlight if any
+box1.addEventListener("MSPointerUp", function(e2){
+	    overX2 = Math.floor(e2.pageX / cw);
+		overY2 = Math.floor(e2.pageY / cw);
+    if(gameboard[overY3-1][overX3-1].contents!=5 && signPressed != 0){
+        //assign the selected sign to the tile at the cursor
+        gameboard[overY3-1][overX3-1].contents = signPressed;
+        signplaceSound.play();
+		e2.preventDefault();
+    }
+    highlight = false;
+    signPressed = 0;
+},false)
+
+box1.addEventListener("MSPointerMove", function(e3){
+    //calculates which tile mouse is currently over
+    overX3 = Math.floor(e3.pageX / cw);
+    overY3 = Math.floor(e3.pageY / cw);
+			e3.preventDefault();
+},false)
+} else{
+		alert("no");
 $("#canvas").on("vmousedown", function(e){
 
     for (var i = 0; i < 4; i++){
@@ -69,3 +113,4 @@ $("#canvas").on("vmousemove", function(e3){
     overY3 = Math.floor(e3.pageY / cw);
 			e3.preventDefault();
 })
+};
